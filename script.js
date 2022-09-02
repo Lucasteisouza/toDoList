812c681824d94a0d2c25077d91873f707383ff2e
Lucasteisouza <lucasteisouza@gmail.com>
2022-09-02 15:34:08 -0300

    Requisito 7 atendido

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const inputText = document.getElementById('texto-tarefa');
 const addTaskButton = document.getElementById('criar-tarefa');
-const taskList = document.getElementById('lista-tarefas')
+const taskList = document.getElementById('lista-tarefas');
 
 addTaskButton.addEventListener('click', addTask);
 
 function addTask() {
     let taskItem = document.createElement('li');
+    taskItem.addEventListener('click', changeToGray)
     taskItem.innerText = inputText.value;
     inputText.value ='';
     taskList.appendChild(taskItem);
+}
+
+function changeToGray(event){
+    event.target.style.backgroundColor = 'gray';
 }
