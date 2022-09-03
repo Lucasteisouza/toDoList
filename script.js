6d0cafb77c56901c3b7587cdd4b9d69e5656151e
Lucasteisouza <lucasteisouza@gmail.com>
2022-09-03 15:43:40 -0300

    Inicia Lintr

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const inputText = document.getElementById('texto-tarefa');
 const addTaskButton = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
-const taskItemListed = document.getElementsByClassName ('taskItem');
+const taskItemListed = document.getElementsByClassName('taskItem');
 const clearTasksButton = document.getElementById('apaga-tudo');
 const removeFinishedTasksButton = document.getElementById('remover-finalizados');
 const completedTasks = document.getElementsByClassName('completed');
@@ -9,78 +9,77 @@ const saveButton = document.getElementById('salvar-tarefas');
 const removeSingleButton = document.getElementById('remover-selecionado');
 const savedListRestored = JSON.parse(localStorage.getItem('savedList'));
 
-
-addTaskButton.addEventListener('click', addTask);
-clearTasksButton.addEventListener('click', clearTasks);
-removeFinishedTasksButton.addEventListener('click', removeFinished);
-saveButton.addEventListener('click', saveList);
-removeSingleButton.addEventListener('click', removeSingleTask);
-
-if (savedListRestored != null){
-    for (index = 0; index < savedListRestored.length; index += 1) {
-        let taskItem = document.createElement('li');
-        taskItem.className = savedListRestored[index].class;
-        taskItem.addEventListener('click', selectItem);
-        taskItem.addEventListener('dblclick', taskCompleted);
-        taskItem.innerText = savedListRestored[index].texto;
-        inputText.value ='';
-        taskList.appendChild(taskItem);
+function selectItem(e) {
+  if (e.target.id === 'selected-item') {
+    e.target.id = '';
+  } else {
+    for (let index = 0; index < taskItemListed.length; index += 1) {
+      taskItemListed[index].id = '';
     }
-}   
-
-function addTask() {
-    let taskItem = document.createElement('li');
-    taskItem.className = 'taskItem';
-    taskItem.addEventListener('click', selectItem);
-    taskItem.addEventListener('dblclick', taskCompleted);
-    taskItem.innerText = inputText.value;
-    inputText.value ='';
-    taskList.appendChild(taskItem);
-}
-
-function selectItem(event) {
-    if(event.target.id === 'selected-item') {
-        event.target.id = '';
-    } else {
-        for(index=0; index < taskItemListed.length; index +=1) {
-            taskItemListed[index].id = '';
-        }
-        event.target.id ='selected-item'
-    }
+    e.target.id = 'selected-item';
+  }
 }
 
 function taskCompleted(event) {
-    if (event.target.classList[1] == 'completed') {
-        event.target.classList.remove('completed')
-    } else {
-        event.target.classList.add('completed');
-    }
+  if (event.target.classList[1] === 'completed') {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
 }
+
+if (savedListRestored != null) {
+  for (let index = 0; index < savedListRestored.length; index += 1) {
+    const taskItem = document.createElement('li');
+    taskItem.className = savedListRestored[index].class;
+    taskItem.addEventListener('click', selectItem);
+    taskItem.addEventListener('dblclick', taskCompleted);
+    taskItem.innerText = savedListRestored[index].texto;
+    inputText.value = '';
+    taskList.appendChild(taskItem);
+  }
+}
+
+function addTask() {
+  const taskItem = document.createElement('li');
+  taskItem.className = 'taskItem';
+  taskItem.addEventListener('click', selectItem);
+  taskItem.addEventListener('dblclick', taskCompleted);
+  taskItem.innerText = inputText.value;
+  inputText.value = '';
+  taskList.appendChild(taskItem);
+}
+addTaskButton.addEventListener('click', addTask);
 
 function clearTasks() {
-    for (index = taskList.children.length -1; index >= 0; index -= 1) {
-        taskList.children[index].remove();
-    }
+  for (let index = taskList.children.length - 1; index >= 0; index -= 1) {
+    taskList.children[index].remove();
+  }
 }
+clearTasksButton.addEventListener('click', clearTasks);
 
 function removeFinished() {
-    for (index = completedTasks.length -1; index >= 0; index -= 1) {
-        completedTasks[index].remove();
-    }
+  for (let index = completedTasks.length - 1; index >= 0; index -= 1) {
+    completedTasks[index].remove();
+  }
 }
+removeFinishedTasksButton.addEventListener('click', removeFinished);
 
 function saveList() {
-    let listObjArr=[];
-    for (let index = 0; index < taskItemListed.length; index += 1) {
-        let listObj = {texto: taskItemListed[index].innerText, class: taskItemListed[index].className};
-        listObjArr.push(listObj);
-    }
-    let stringedObjArr = JSON.stringify(listObjArr);
-    localStorage.setItem('savedList', stringedObjArr);
+  const listObjArr = [];
+  for (let index = 0; index < taskItemListed.length; index += 1) {
+    const list = { texto: taskItemListed[index].innerText, class: taskItemListed[index].className };
+    listObjArr.push(list);
+  }
+  const stringedObjArr = JSON.stringify(listObjArr);
+  localStorage.setItem('savedList', stringedObjArr);
 }
-function removeSingleTask(){
-    const selectedTask = document.getElementById('selected-item');
-    if(selectedTask != undefined) {
-        selectedTask.remove();
-    }
+saveButton.addEventListener('click', saveList);
+
+function removeSingleTask() {
+  const selectedTask = document.getElementById('selected-item');
+  if(selectedTask != undefined) {
+    selectedTask.remove();
+  }
 }
+removeSingleButton.addEventListener('click', removeSingleTask);
